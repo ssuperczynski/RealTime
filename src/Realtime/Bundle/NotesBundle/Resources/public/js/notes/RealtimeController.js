7cd475e4047cf9bebb76d3d27f9bd163eaf2b985
Sebastian Superczynski <sebastian.superczynski@espeo.pl>
2014-11-10 17:12:54 +0100

    html fix

--- src/Realtime/Bundle/NotesBundle/Resources/public/js/notes/RealtimeController.js
+++ src/Realtime/Bundle/NotesBundle/Resources/public/js/notes/RealtimeController.js
@@ -4,14 +4,13 @@ app.controller('realtimeController', ['$scope', 'RealtimeService', function ($sc
 
         $scope.send = function (content) {
             RealtimeService.send(content);
+            console.log($scope.notes);
         };
        
         $scope.get = function () {
             RealtimeService.get().then(function(){
-                console.log('get', $scope.notes.data);
+                console.log('get:', $scope.notes);
             });
-
-
         };
 
         $scope.notes = RealtimeService.notes;
